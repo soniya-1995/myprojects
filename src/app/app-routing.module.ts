ba4caf52e7e2a65329e76c42826bf50fa35485ff
soniya-1995 <websoullabssoniya@gmail.com>
2020-09-11 15:20:34 +0530

    header work completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "header",
+    component :HeaderComponent
+
+  },
+
+  {
+    path:"login",
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
